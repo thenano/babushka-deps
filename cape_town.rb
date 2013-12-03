31d02a146d578276c1914e5c0e6a4f4fc7ab5262
Sam Gibson <sam@ifdown.net>
2013-12-03 09:37:58 +0200

    Set hostname

--- cape_town.rb
+++ cape_town.rb
@@ -1,6 +1,17 @@
+dep 'hostname' do
+  met? {
+    shell("hostname").include? "cape-town"
+  }
+
+  meet {
+    shell("sudo scutil --set HostName cape-town")
+  }
+end
+
 dep 'cape-town' do
   requires 'homebrew'
   requires 'all-packaged-apps'
+  requires 'hostname'
   requires 'dots'
   requires 'all-osx-settings'
   requires 'all-osx-apps'
