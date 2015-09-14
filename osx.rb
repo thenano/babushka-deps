993215803dcec03ceac33053d3c214c74b1c4d9d
Fernando Freire <thenano@gmail.com>
2015-09-14 16:01:42 +1000

    update apps and settings

--- osx.rb
+++ osx.rb
@@ -3,11 +3,11 @@ dep 'MenuMeters.app' do
 end
 
 dep 'VLC.app' do
-  source "http://get.videolan.org/vlc/2.2.0/macosx/vlc-2.2.0.dmg"
+  source "http://get.videolan.org/vlc/2.2.1/macosx/vlc-2.2.1.dmg"
 end
 
 dep 'VirtualBox.installer' do
-  source "http://download.virtualbox.org/virtualbox/4.3.8/VirtualBox-4.3.8-92456-OSX.dmg"
+  source "http://download.virtualbox.org/virtualbox/5.0.4/VirtualBox-5.0.4-102546-OSX.dmg"
 end
 
 dep 'Vagrant.app' do
@@ -17,39 +17,27 @@ dep 'Vagrant.app' do
     "/usr/bin/vagrant".p.exists?
   }
 
-  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.2.dmg"
+  source "https://dl.bintray.com/mitchellh/vagrant/vagrant_1.7.4.dmg"
 end
 
 dep 'Dropbox.app' do
   source "https://www.dropbox.com/download?plat=mac"
 end
 
-dep 'Alfred.app' do
-  source "https://cachefly.alfredapp.com/Alfred_2.6_374.zip"
-end
-
-dep 'iTerm.app' do
-  source "https://iterm2.com/downloads/stable/iTerm2_v2_0.zip"
-end
-
-dep 'Spectacle.app' do
-  source "https://s3.amazonaws.com/spectacle/downloads/Spectacle+0.8.8.zip"
-end
-
-dep 'Chromium.app' do
-  source "https://commondatastorage.googleapis.com/chromium-browser-snapshots/Mac/273149/chrome-mac.zip"
+dep 'Chrome.app' do
+  source "http://dl.google.com/chrome/mac/dev/GoogleChrome.dmg"
 end
 
 dep 'FirefoxDeveloperEdition.app' do
-  source "https://download-installer.cdn.mozilla.net/pub/firefox/nightly/latest-mozilla-aurora/firefox-38.0a2.en-US.mac.dmg"
+  source "https://download.mozilla.org/?product=firefox-aurora-latest-ssl&os=osx&lang=en-US"
 end
 
 dep 'Transmission.app' do
   source 'https://transmission.cachefly.net/Transmission-2.84.dmg'
 end
 
-dep 'MacVim.app' do
-  source 'https://github.com/b4winckler/macvim/releases/download/snapshot-73/MacVim-snapshot-73-Mavericks.tbz'
+dep 'SublimeText.app' do
+  source 'http://c758482.r82.cf2.rackcdn.com/Sublime%20Text%202.0.2.dmg'
 end
 
 dep 'all-osx-apps' do
@@ -57,14 +45,11 @@ dep 'all-osx-apps' do
   requires 'VLC.app'
   requires 'VirtualBox.installer'
   requires 'Vagrant.app'
-  requires 'Spectacle.app'
-  requires 'iTerm.app'
-  requires 'Alfred.app'
   requires 'Dropbox.app'
-  requires 'Chromium.app'
+  requires 'Chrome.app'
   requires 'Transmission.app'
   requires 'FirefoxDeveloperEdition.app'
-  requires 'MacVim.app'
+  requires 'SublimeText.app'
 end
 
 dep 'enable-full-disk-encryption' do
@@ -85,17 +70,6 @@ dep 'set-dock-magnification' do
 
   meet {
     shell("defaults write com.apple.dock magnification -integer 1")
-  }
-end
-
-dep 'move-dock-right' do
-  met? {
-    shell?("defaults read com.apple.dock orientation") &&
-      shell("defaults read com.apple.dock orientation") == "right"
-  }
-
-  meet {
-    shell("defaults write com.apple.dock orientation -string 'right'")
   }
 end
 
@@ -135,37 +109,8 @@ dep 'fast-key-repeat' do
   }
 end
 
-dep 'capslock-to-ctrl' do
-  def vendor_and_product_id
-    keyboard_info = shell("ioreg -n IOHIDKeyboard -r")
-    vendor_id = keyboard_info.scan(/"VendorID" = (\d+)/).flatten.first
-    product_id = keyboard_info.scan(/"ProductID" = (\d+)/).flatten.first
-
-    [vendor_id, product_id]
-  end
-
-  met? {
-    vendor_id, product_id = vendor_and_product_id
-    cmd = "defaults -currentHost read -g com.apple.keyboard.modifiermapping.#{vendor_id}-#{product_id}-0"
-    shell?(cmd) && shell(cmd) ==
-%Q{(
-        {
-        HIDKeyboardModifierMappingDst = 2;
-        HIDKeyboardModifierMappingSrc = 0;
-    }
-)}
-  }
-
-  meet {
-    vendor_id, product_id = vendor_and_product_id
-    shell("defaults -currentHost write -g com.apple.keyboard.modifiermapping.#{vendor_id}-#{product_id}-0 -array '<dict><key>HIDKeyboardModifierMappingSrc</key><integer>0</integer><key>HIDKeyboardModifierMappingDst</key><integer>2</integer></dict>'")
-  }
-end
-
 dep 'all-osx-settings' do
-  requires 'capslock-to-ctrl'
   requires 'fast-key-repeat'
   requires 'disable-widgets'
-  requires 'move-dock-right'
   requires 'auto-hide-dock'
 end
