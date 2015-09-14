64fc2c47a7d169d5e2e81352ea65ff05f4386ae0
Fernando Freire <thenano@gmail.com>
2015-09-14 15:30:17 +1000

    remove zsh, vim and vundle

--- apps.rb
+++ apps.rb
@@ -1,6 +1,4 @@
 packaged_apps = %w{
-  zsh
-  vim
   pass
   wget
   tree
@@ -11,48 +9,4 @@ packaged_apps.each do |app|
   dep "#{app}.bin" do
     installs app
   end
-end
-
-dep "homebrew-games" do
-  met? {
-    shell("brew tap").include? "homebrew/games"
-  }
-
-  meet {
-    shell("brew tap homebrew/games")
-  }
-end
-
-games = %w{
-  nethack
-}
-
-games.each do |app, ps|
-  dep "#{app}.bin" do
-    requires "homebrew-games"
-    installs app
-    provides ps
-  end
-end
-
-dep 'all-packaged-apps' do
-  requires *(packaged_apps).map { |a| "#{a}.bin" }
-  requires 'vundle plugins up to date.vim'
-  requires 'vundle up to date.repo'
-end
-
-dep 'vundle plugins up to date.vim' do
-  requires 'vundle up to date.repo'
-
-  met? { shell? 'vim +PluginInstall! +PluginClean! +qall 2&> /dev/null' }
-end
-
-dep 'vundle up to date.repo' do
-  met? {
-    '~/.vim/bundle/Vundle.vim'.p.exists?
-  }
-
-  meet {
-    git "https://github.com/gmarik/Vundle.vim", :to => '~/.vim/bundle/Vundle.vim'
-  }
 end
