7d2e30c082db1bb409af78e564e9126d0dd74522
Fernando Freire <ffreire@thoughtworks.com>
2018-03-22 01:46:22 +1100

    kubernetes and aws related deps

--- apps.rb
+++ apps.rb
@@ -6,6 +6,7 @@ packaged_apps = %w{
   ruby-build
   pyenv
   yarn
+  kops
 }
 
 packaged_apps.each do |app|
@@ -35,6 +36,21 @@ dep 'blackbox.bin' do
   provides 'blackbox_initialize'
 end
 
+dep 'awscli.bin' do
+  installs 'awscli'
+  provides 'aws'
+end
+
+dep 'kubernetes-cli.bin' do
+  installs 'kubernetes-cli'
+  provides 'kubectl'
+end
+
+dep 'kubernetes-helm.bin' do
+  installs 'kubernetes-helm'
+  provides 'helm'
+end
+
 dep 'bash-completion.bin' do
   met? {
     (shell('brew --prefix') + '/etc/bash_completion').p.exists?
@@ -47,6 +63,9 @@ dep 'all-packaged-apps' do
   requires *(packaged_apps).map { |a| "#{a}.bin" }
   requires 'bash-completion.bin'
   requires 'blackbox.bin'
+  requires 'awscli.bin'
+  requires 'kubernetes-cli.bin'
+  requires 'kubernetes-helm.bin'
   requires 'nvm.bin'
   requires 'git.bin'
 end
